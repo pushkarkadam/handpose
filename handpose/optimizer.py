adfc26a89b5770128895d4ed63da82793381bfb5
Pushkar Kadam <pushkarkadam17@outlook.com>
2024-06-06 11:50:47 +0100

    Using Optimizer class to use zero_grad() method

--- handpose/optimizer.py
+++ handpose/optimizer.py
@@ -1,3 +1,7 @@
+import torch
+import warnings
+
+
 def optimizer_fn(model, optimizer, **kwargs):
     """Returns an optimizer.
 
@@ -23,6 +27,10 @@ def optimizer_fn(model, optimizer, **kwargs):
     >>> optimizer_fn(model, optim.SGD, **{'lr':0.01, 'momentum':0.9})
     
     """
+    warnings.warn(
+        r"`optimizer_fn()` is deprecated and will be removed in future version. Please use the `handpose.optimizer.Optimizer` class instead.",
+        DeprecationWarning
+    )
 
     # Using parameters whose weights are not frozen
     params = [param for param in model.parameters() if param.requires_grad]
@@ -30,3 +38,71 @@ def optimizer_fn(model, optimizer, **kwargs):
     opt = optimizer(params, **kwargs)
 
     return opt
+
+class Optimizer(torch.nn.Module):
+    """
+    A wrapper class for optimizers to be used with a PyTorch model.
+
+    This class simplifies the creation of an optimizer by automatically
+    filtering out model parameters that do not require gradients.
+
+    Select optimizer from different algorithms in `Optimizer`_.
+
+    .. _Optimizer: https://pytorch.org/docs/stable/optim.html#algorithms
+
+
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The neural network model whose parameters will be optimized.
+    optimizer_cls : type
+        The optimizer class from `torch.optim` (e.g., `torch.optim.SGD`).
+    **kwargs : dict
+        Additional keyword arguments to be passed to the optimizer.
+
+    Attributes
+    ----------
+    model : torch.nn.Module
+        The neural network model to be optimized.
+    optimizer : torch.optim.Optimizer
+        The optimizer instance that will be used to update the model's parameters.
+
+    Methods
+    -------
+    __init__(model, optimizer_cls, **kwargs)
+        Initializes the Optimizer instance with a given model and optimizer class.
+
+    Examples
+    --------
+    >>> model = MyModel()
+    >>> optimizer = Optimizer(model=model, optimizer_cls=torch.optim.SGD, lr=0.01, momentum=0.9)
+    >>> optimizer.optimizer.step()  # Perform an optimization step
+    """
+    
+    def __init__(self, model, optimizer_cls, **kwargs):
+        """
+        Initializes the Optimizer instance.
+
+        Parameters
+        ----------
+        model : torch.nn.Module
+            The model to be optimized.
+        optimizer_cls : type
+            The optimizer class from `torch.optim` (e.g., `torch.optim.SGD`).
+        **kwargs : dict
+            Additional keyword arguments to be passed to the optimizer.
+
+        Examples
+        --------
+        >>> model = MyModel()
+        >>> optimizer = Optimizer(model=model, optimizer_cls=torch.optim.SGD, lr=0.01, momentum=0.9)
+        >>> optimizer.optimizer.zero_grad()
+        """
+        super().__init__()
+        self.model = model
+
+        # Filter parameters that require gradients
+        params = filter(lambda p: p.requires_grad, model.parameters())
+        
+        # Initialize the optimizer with the filtered parameters
+        self.optimizer = optimizer_cls(params, **kwargs)
--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -111,3 +111,55 @@ def test_optimizer_fn3():
     assert (optimizer.param_groups[0]['lr'] == 0.01)
     assert (optimizer.param_groups[0]['momentum'] == 0.9)
     assert (len(optimizer.param_groups[0]['params']) == 16)
+
+class SimpleModel(torch.nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+        self.fc1 = torch.nn.Linear(10, 5)
+        self.fc2 = torch.nn.Linear(5, 2)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def test_optimizer_initialization():
+    model = SimpleModel()
+    optimizer_cls = optim.SGD
+    optimizer = handpose.optimizer.Optimizer(model=model, optimizer_cls=optimizer_cls, lr=0.01, momentum=0.9)
+
+    assert isinstance(optimizer, handpose.optimizer.Optimizer)
+    assert isinstance(optimizer.optimizer, optim.SGD)
+    assert optimizer.optimizer.defaults['lr'] == 0.01
+    assert optimizer.optimizer.defaults['momentum'] == 0.9
+
+def test_optimizer_parameters():
+    model = SimpleModel()
+    optimizer_cls = optim.Adam
+    optimizer = handpose.optimizer.Optimizer(model=model, optimizer_cls=optimizer_cls, lr=0.001)
+
+    param_groups = optimizer.optimizer.param_groups
+    for group in param_groups:
+        for param in group['params']:
+            assert param.requires_grad
+
+def test_optimizer_step():
+    model = SimpleModel()
+    optimizer_cls = optim.SGD
+    optimizer = handpose.optimizer.Optimizer(model=model, optimizer_cls=optimizer_cls, lr=0.01)
+
+    # Forward pass
+    input_tensor = torch.randn(1, 10)
+    output = model(input_tensor)
+
+    # Backward pass
+    loss = output.sum()
+    loss.backward()
+
+    # Perform an optimization step
+    optimizer.optimizer.step()
+
+    # Check if the parameters were updated (i.e., gradients are no longer zero)
+    for param in model.parameters():
+        if param.grad is not None:
+            assert torch.any(param.grad != 0)
