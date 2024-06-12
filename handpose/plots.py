d81e6f2019b58f6f09b3795395c41aae76c92698
Pushkar Kadam <pushkarkadam17@outlook.com>
2024-06-12 23:43:57 +0100

    plots name changed and new function parameters added

--- handpose/plots.py
+++ handpose/plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-def plot_loss_history(loss_history, root_path = './', save_path_prefix='loss_plot'):
+def plot_single_history(history, root_path = './', save_path_prefix='loss_plot', xlabel='Epoch', ylabel='Loss'):
     """
     Plot individual line graphs for each key in the input dictionary.
     Titles indicate the type ('Train' or 'Valid') and the specific loss type.
@@ -10,20 +10,29 @@ def plot_loss_history(loss_history, root_path = './', save_path_prefix='loss_plo
 
     Parameters
     ----------
-    loss_history: dict 
-        Dictionary containing loss history data.
+    history: dict 
+        Dictionary containing history data.
     root_path: str, default ``'./'``
         Root path to store the plots
     save_path_prefix: str, default ``'loss_plot'``
         Prefix for saving the images.
+    xlabel: str, default ``'Epoch'``
+        X axis label.
+    ylabel: str, default ``'Loss'``
+        Y axis label.
+
+    Examples
+    --------
+    >>> mAP_history = {'train': {'mAP': [1.0, 0.5]}, 'valid': {'mAP': [1.0, 0.5]}}
+    >>> plot_single_history(history=mAP_history, root_path='./', save_path_prefix='', xlabel='Epoch', ylabel='mAP')
     
     """
-    for loss_type, data in loss_history.items():
+    for loss_type, data in history.items():
         for split_type, losses in data.items():
             plt.plot(losses, marker='o')
             plt.title(f'{split_type.capitalize()}: {loss_type}')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
             plt.tight_layout()
             save_path = f'{save_path_prefix}_{split_type}_{loss_type}.png'
             train_save_path = os.path.join(root_path, save_path)
@@ -31,33 +40,44 @@ def plot_loss_history(loss_history, root_path = './', save_path_prefix='loss_plo
             plt.close()
 
 
-def all_loss_history(loss_history, root_path='./', plot_name='loss_plot.png'):
-    """Creates a combined loss plot
+def plot_all_history(history, root_path='./', plot_name='loss_plot.png', xlabel='Epoch', ylabel='Loss'):
+    """Creates a combined history plot
+
+    This can be used to view all the loss or mAP plots in the same chart.
 
     Parameters
     ----------
-    loss_history: dict 
-        Dictionary containing loss history data.
+    history: dict 
+        Dictionary containing history data.
     root_path: str, default ``'./'``
         Root path to store the plots
-    save_path_prefix: str, default ``'loss_plot'``
-        Prefix for saving the images.
+    plot_name: str, default ``'loss_plot.png'``
+        Name of the plot used for saving.
+    xlabel: str, default ``'Epoch'``
+        X axis label.
+    ylabel: str, default ``'Loss'``
+        Y axis label.
+
+    Examples
+    --------
+    >>> loss_history = {'train': {'total_loss': [0.8,0.9], 'box_loss': [0.8, 0.9]}, 'valid': {'total_loss': [0.8,0.9], 'box_loss': [0.8, 0.9]}}
+    >>> plot_all_history(history=loss_history, root_path='./', plot_name='loss_plot.png', xlabel='Epoch', ylabel='Loss')
 
     """
     # Create subplots
-    fig, axs = plt.subplots(2, len(loss_history['train']), figsize=(12, 6))
+    fig, axs = plt.subplots(2, len(history['train']), figsize=(12, 6))
 
     # Plot 'train' and 'valid' graphs for each key
-    for i, key in enumerate(loss_history['train']):
-        axs[0, i].plot(loss_history['train'][key], label='train', marker='o')
+    for i, key in enumerate(history['train']):
+        axs[0, i].plot(history['train'][key], label='train', marker='o')
         axs[0, i].set_title(f'Train: {key}')
-        axs[0, i].set_xlabel('Epoch')
-        axs[0, i].set_ylabel('Loss')
+        axs[0, i].set_xlabel(xlabel)
+        axs[0, i].set_ylabel(ylabel)
         
-        axs[1, i].plot(loss_history['valid'][key], label='valid', marker='o')
+        axs[1, i].plot(history['valid'][key], label='valid', marker='o')
         axs[1, i].set_title(f'Valid: {key}')
-        axs[1, i].set_xlabel('Epoch')
-        axs[1, i].set_ylabel('Loss')
+        axs[1, i].set_xlabel(xlabel)
+        axs[1, i].set_ylabel(ylabel)
 
     # Adjust layout
     plt.tight_layout()
@@ -65,3 +85,4 @@ def all_loss_history(loss_history, root_path='./', plot_name='loss_plot.png'):
     # Save plot as an image
     plot_save_path = os.path.join(root_path, plot_name)
     plt.savefig(plot_save_path)
+    plt.close()
